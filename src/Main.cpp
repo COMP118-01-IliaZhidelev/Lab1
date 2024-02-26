ff85f957024700290d24313afaa16b5077637531
Your Name <you@example.com>
2024-02-26 10:41:31 +0200

    add draw square

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <cassert>
-#include <string>
+#include <sstream>
 
 void drawHorizontalLine(int length, char ch);
 void drawVerticalLine(int height, char ch);
@@ -24,9 +24,38 @@ void drawVerticalLine(int height, char ch)
 		std::cout << ch << "\n";
 	}
 }
+// example: *             *
+void drawLineEmpty(int length,char ch)
+{
+	std::stringstream ss;
+	std::cout << ch;
+	std::string whiteSpaces(length, ' ');
+	ss << whiteSpaces;
+	std::cout << ss.str();
+	std::cout << ch;
+	std::cout << std::endl;
+}
 void drawSquare(int size, char ch)
 {
+	assert(size> 0);
 	std::cout << "I am drawSquare\n";
+
+	//first line
+	std::string topLine(size, ch);
+	std::cout << topLine << std::endl;
+	
+	for (int i = 0; i < size-2; i++)
+	{
+		drawLineEmpty(size - 2, ch);
+	}
+
+	// last line
+
+	std::string bottomLine(size, ch);
+	std::cout << bottomLine << std::endl;
+
+
+
 }
 void drawRectangle(int height, int length, char ch)
 {
@@ -64,9 +93,14 @@ int main()
 		int height = getInt("Height:");
 		drawVerticalLine(height, '*');
 	}
+	else if (option == 3)
+	{
+		int size = getInt("Size:");
+		drawSquare(size, '*');
+	}
 	else
 	{
-		assert(false);
+		assert(false && "could not parse input");
 	}
 
 
